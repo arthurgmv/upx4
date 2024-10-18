3b2c3ec8f5c40f72ab77fe9c7852791b34fe9fed
Arthur Gabriel <arthurgabrielmv@gmail.com>
2024-10-17 23:33:14 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {Card01Component} from './card01/card01.component';
+import {Card02Component} from './card02/card02.component';
+import {Card03Component} from './card03/card03.component';
+import {Card04Component} from './card04/card04.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, Card01Component, Card02Component, Card03Component, Card04Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
